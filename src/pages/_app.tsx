7a09740595e8430f1040a417f56bfff6c07fe24a
skstef <sstefstefv@gmail.com>
2025-04-10 08:25:37 +0300

    feature: favicon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,24 @@ import "../styles/globals.css"; // Import global styles (e.g., Tailwind CSS)
 import "../config/i18n";
 import { appWithTranslation } from "next-i18next";
 import { AppGuarg } from "@/components/shared/AppGuard";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <AppGuarg>
-        <Component {...pageProps} />
-      </AppGuarg>
-    </Provider>
+    <>
+      <Head>
+        <title>PDD Moldova</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5"
+        />
+      </Head>
+      <Provider store={store}>
+        <AppGuarg>
+          <Component {...pageProps} />
+        </AppGuarg>
+      </Provider>
+    </>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="PDD Moldova" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
